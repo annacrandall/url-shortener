3731c6bda0ace2ca3be78a0ac2f1463410e3c29f
ana crandall <107437409+annacrandall@users.noreply.github.com>
2022-12-20 23:38:54 +0000

    Set up required packages.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const mongoDB = require('mongodb'); 
+const mongoose = require('mongoose'); 
+const bodyParser = require('body-parser'); 
+const app = express(); 
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}); 
+//set up mongoose package 
 
 app.use(cors());
 
@@ -14,11 +19,16 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const shortURL = mongoose.model('shortURL', new mongoose.Schema({
+  original_url : String, 
+  short_url: String,
+})); 
+// create schema + model to store/save input url's 
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+  res.json({ greeting: 'hello freeCodeCamp cert' });
 });
-
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
